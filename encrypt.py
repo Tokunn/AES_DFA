40be17e00d52fac3cd9d434e118cd42fe5cab3f4
Tokunn <kaseikappatokunn2@yahoo.co.jp>
2021-03-02 17:48:00 +0900

    add decrypt

--- encrypt.py
+++ encrypt.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 import numpy as np
-from tables import sbox_table, gal, Rcon
+from tables import sbox_table, gal, Rcon, inv_sbox_table, inv_gal
 gal = np.asarray(gal)
+inv_gal = np.asarray(inv_gal)
 np.set_printoptions(formatter={'int':'{:02x}'.format})
 
 DEBUG_PRINT_ROUNDKEY_GEN = False
 DEBUG_PRINT_ENCRIPT = True
+DEBUG_PRINT_DECRYPT = False
 
 def print_rkey(*arg):
     if DEBUG_PRINT_ROUNDKEY_GEN:
         print("[DEBUG] rkey ", *arg)
+
+def print_dec(*arg):
+    if DEBUG_PRINT_DECRYPT:
+        print("[DEBUG] dec ", arg[0]+'\n', *arg[1:])
 
 def print_enc(*arg):
     if DEBUG_PRINT_ENCRIPT:
@@ -75,6 +81,16 @@ def SubBytes(state):
     print_enc("After SubBytes", r)
     return r
 
+def invSubBytes(state):
+    # invSBox Tableで変換
+    state = state.reshape(-1)
+    new_state = np.zeros_like(state)
+    for i in range(state.shape[0]):
+        new_state[i] = inv_sbox_table[state[i]]
+    r = new_state.reshape(4,4)
+    print_dec("After invSubBytes", r)
+    return r
+
 def ShiftRows(state):
     # 行数に応じて横方向にシフト
     new_state = np.zeros_like(state)
@@ -85,20 +101,32 @@ def ShiftRows(state):
     print_enc("After ShiftRows", new_state)
     return new_state
 
+def invShiftRows(state):
+    # 行数に応じて横方向にシフト
+    new_state = np.zeros_like(state)
+    new_state[0] = state[0]
+    for i in range(1, state.shape[0]):
+        new_state[i, :i] = state[i, -i:]
+        new_state[i, i:] = state[i, :-i]
+    print_enc("After invShiftRows", new_state)
+    return new_state
+
 def gmult(a, b):
+    # aの最下位ビットが立ってるかを確認して、立っている場合には結果にbをかける(XOR)
+    # 次のビットを見るためにBは右シフト
+    # xをかけた値を作るためにAは左シフト
     # x**8 + x**4 + x**3 + x + 1 を法とするGF(2)における積
-    # 1ならそのまま、xなら1bit左シフト、(x+1)なら両方の和（GF(2)ではXOR）
     # 8bitを超えたらオーバーフロー処理
     # x**8 = -x**4 - x**3 - x -1 を代入して計算
-    assert (1<=a) or (a<=3)
-    if (a==1): # 1
-        c = b
-    elif ((a==2) or (a==3)): # x or (x + 1)
-        c = b << 1
-        if (c&0x100):
-            c ^= 0b000100011011 # x**8 + x**4 + x**3 + x + 1  # Overflow check
-        if (a==3): # (x + 1)
-            c ^= b
+
+    c = 0
+    for i in range(8):
+        if (b & 0b1): # 最下位ビットが立っているなら
+            c ^= a 
+        b >>= 1
+        a <<= 1
+        if (a & 0x100): # オーバーフローしているなら
+            a ^= 0b100011011 # x**8 + x**4 + x**3 + x + 1  
 
     return c
 
@@ -137,8 +165,16 @@ def MixColumns(state):
         new_state[:,i] = xor_dot(gal, state[:,i])
     print_enc("After MixColumns", new_state)
     return new_state
+    
+def invMixColumns(state):
+    # 縦方向の列ごとに行列演算(足し算ではなくXOR)
+    new_state = np.zeros_like(state)
+    for i in range(state.shape[1]):
+        new_state[:,i] = xor_dot(inv_gal, state[:,i])
+    print_enc("After invMixColumns", new_state)
+    return new_state
 
-def main(p_text, o_key):
+def encrypt(p_text, o_key):
 
     # Cast text and key
     # 縦に並べる
@@ -166,14 +202,47 @@ def main(p_text, o_key):
     state = ShiftRows(state)
     state = AddRoundKey(state, ext_key[10])
 
-    print('='*20+"\nOUTPUT\n", state)
+    print('='*20+"\nENC OUTPUT\n", state)
+    return state.T.reshape(-1)
+
+def decrypt(c_text, o_key):
+
+    # Cast text and key
+    # 縦に並べる
+    state = np.asarray(c_text).reshape(4, 4).T
+    # 縦に並べる
+    key = np.asarray(o_key).reshape(4, 4).T
+    
+    # Extend Key
+    ext_key = make_extkey(key)
+
+    print_dec('-'*20+"\nStart of Round", state)
+    state = AddRoundKey(state, ext_key[10])
+    state = invShiftRows(state)
+    state = invSubBytes(state)
+
+    for i in range(9, 0, -1): # 9times [9,8,...2,1]
+        print_dec('-'*20+"\nStart of {} Round".format(i), state)
+        state = AddRoundKey(state, ext_key[i])
+        state = invMixColumns(state)
+        state = invShiftRows(state)
+        state = invSubBytes(state)
+
+    state = AddRoundKey(state, ext_key[0])
+
+    print('='*20+"\nDEC OUTPUT\n", state)
+    return state.T.reshape(-1)
 
 if '__main__' == __name__:
     # plane_text = 0x3243f6a8885a308d313198a2e0370734
     # original_key = 0x2b7e151628aed2a6abf7158809cf4f3c
-    plane_text = [0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34]
     original_key = [0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c]
-    main(plane_text, original_key)
+
+    plane_text = [0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34]
+    cipher_text = encrypt(plane_text, original_key)
+
+    # cipher_text = [0x39, 0x25, 0x84, 0x1d, 0x02, 0xdc, 0x09, 0xfb, 0xdc, 0x11, 0x85, 0x97, 0x19, 0x6a, 0x0b, 0x32]
+    print(decrypt(cipher_text, original_key))
 
     # test = np.asarray([[0, 1, 2, 3],
     #                    [4, 5, 6, 7],
